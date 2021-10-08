a34baae75bdc650689475facd44c73d98e620519
Xiaoming Fu <xmingfu86@gmail.com>
2021-10-07 23:14:19 -0700

    Connect the dots in server-land

--- odyssey-lift-off-part1/server/src/index.js
+++ odyssey-lift-off-part1/server/src/index.js
@@ -1,25 +1,16 @@
 const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./schema");
 
-const mocks = {
-  Track: () => ({
-    id: () => "track_01",
-    title: () => "Astro Kitty, Space Explorer",
-    author: () => {
-      return {
-        name: "Grumpy Cat",
-        photo:
-          "https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg",
-      };
-    },
-    thumbnail: () =>
-      "https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg",
-    length: () => 1210,
-    modulesCount: () => 6,
-  }),
-};
+const resolver = require("./resolvers");
+const TrackAPI = require("./dataSources/track-api");
 
-const server = new ApolloServer({ typeDefs, mocks });
+const server = new ApolloServer({
+  typeDefs,
+  resolver,
+  dataSources: () => {
+    return { trackAPI: new TrackAPI() };
+  },
+});
 
 server.listen().then(() => {
   console.log(`
