0f242a78dc88da4cfe927754441a0e31de7b2f27
Xiaoming Fu <xmingfu86@gmail.com>
2021-10-10 23:15:53 -0700

    Resolve a mutation with errors

--- odyssey-lift-off-part1/server/src/resolvers.js
+++ odyssey-lift-off-part1/server/src/resolvers.js
@@ -9,13 +9,22 @@ const resolvers = {
   },
   Mutation: {
     incrementTrackViews: async (_, { id }, { dataSources }) => {
-      const track = await dataSources.trackAPI.getTrack(id);
-      return {
-        code: 200,
-        success: true,
-        message: `successfully increment number of views for track ${id}`,
-        track: track,
-      };
+      try {
+        const track = await dataSources.trackAPI.incrementTrackViews(id);
+        return {
+          code: 200,
+          success: true,
+          message: `Successfully incremented number of views for track ${id}`,
+          track,
+        };
+      } catch (err) {
+        return {
+          code: err.extensions.response.status,
+          success: false,
+          message: err.extensions.response.body,
+          track: null,
+        };
+      }
     },
   },
   Track: {
